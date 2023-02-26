3dbae4cba8b36516ec66053fbc243a9a8d7a57c6
waterfog9580 <waterfog9580@gmail.com>
2023-02-26 10:54:07 +0900

    :sparkles: synchronized 키워드 사용

--- src/main/kotlin/com/waterfogsw/cucurrentsolutions/service/StockService.kt
+++ src/main/kotlin/com/waterfogsw/cucurrentsolutions/service/StockService.kt
@@ -2,20 +2,20 @@ package com.waterfogsw.cucurrentsolutions.service
 
 import com.waterfogsw.cucurrentsolutions.domain.StockRepository
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class StockService(
   private val stockRepository: StockRepository
 ) {
 
-  @Transactional
+  @Synchronized
   fun decrease(id: Long, quantity: Long) {
     val persistStock = stockRepository
         .findById(id)
         .orElseThrow { RuntimeException("Not found") }
 
     persistStock.decrease(quantity)
+    stockRepository.saveAndFlush(persistStock)
   }
 
 }
